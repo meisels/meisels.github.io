fd5c20ceb4cb267926e5c20838f624e9599b7a44
arimeisels3@gmail.com <arimeisels3@gmail.com>
2020-11-24 19:23:31 -0500

    Keepvid Bookmarklet
    A bookmarklet that sends a YouTube video to the deep dark processors of Keepvid's website from which it will never return!!!! HAHAHAHAHAHAHAHAHAHA!!!!!! Occasioanl updates will be realeased as needed. Keepvid, you are free to use this if you give me (Ari Meisels) credit. And no offense but your broken bookmarklet sucks.

--- keepvid/bookmarklet.js
+++ keepvid/bookmarklet.js
@@ -1,13 +1,9 @@
-//My Keepvid Bookmarklet of friendliness!!!!
-//My school blocks YouTube so all of my teachers use the Keepvid website.
-//This tool should make them very happy (more than they are already with me :))
-
-if (window.location.host == "youtube.com") {
+if (window.location.host == "www.youtube.com") {
 	alert("Do you know the difference between YouTube and a YouTube VIDEO????????");
 } else {
 	if (window.location.host + window.location.pathname == "www.youtube.com/watch") {
 		if (document.getElementById("reason") == null) {
-			window.location.replace("https://keepvid.works/?url=" + window.location);
+			window.open("https://keepvid.works/?url=" + window.location);
 		} else {
 			alert("Error 404: The video you are looking for was lost, stolen, or may never have existed. But in all probability, it was probably taken down or made private. Unless you are trying to break my precious bookmarklet, in which case you should be ashamed of yourself!!!!!");
 		}
