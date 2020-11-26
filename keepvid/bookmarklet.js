0f6af39cec80f16f2c7987c03e9858c7a0d98a05
arimeisels3@gmail.com <arimeisels3@gmail.com>
2020-11-25 19:24:44 -0500

    Update bookmarklet.js

--- keepvid/bookmarklet.js
+++ keepvid/bookmarklet.js
@@ -3,16 +3,12 @@ if ((window.location.host + window.location.pathname == "www.youtube.com/") || (
 } else {
 	if (window.location.host + window.location.pathname == "www.youtube.com/watch") {
 		if (document.getElementById("reason") == null) {
-			window.location.replace("https://beta.5blevels.com"); //keepvid.works/?url=" + window.location);
-			setTimeout(pressButton, 3000);
+			var myWindow = window.open("https://keepvid.works/?url=" + window.location);
+			myWindow.document.getElementsByClassName("btn btn-lg btn-danger")[0].click();
 		} else {
 			alert("Error 404: The video you are looking for was lost, stolen, or may never have existed. But in all probability, it was probably taken down or made private. Unless you are trying to break my precious bookmarklet, in which case you should be ashamed of yourself!!!!!");
 		}
 	} else {
 		alert("Please click this on a YouTube video ONLY!!!!!!");
 	}
-};
-
-function pressButton() {
-	alert("hi");
 };
