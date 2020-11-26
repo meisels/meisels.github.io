a38dd7d6b7d5b53411a436fe7305ba749bedda17
arimeisels3@gmail.com <arimeisels3@gmail.com>
2020-11-25 19:14:17 -0500

    Update bookmarklet.js

--- keepvid/bookmarklet.js
+++ keepvid/bookmarklet.js
@@ -4,11 +4,15 @@ if ((window.location.host + window.location.pathname == "www.youtube.com/") || (
 	if (window.location.host + window.location.pathname == "www.youtube.com/watch") {
 		if (document.getElementById("reason") == null) {
 			window.location.replace("https://keepvid.works/?url=" + window.location);
-			setTimeout(alert("hi"), 3000);
+			setTimeout(pressButton, 3000);
 		} else {
 			alert("Error 404: The video you are looking for was lost, stolen, or may never have existed. But in all probability, it was probably taken down or made private. Unless you are trying to break my precious bookmarklet, in which case you should be ashamed of yourself!!!!!");
 		}
 	} else {
 		alert("Please click this on a YouTube video ONLY!!!!!!");
 	}
+};
+
+function pressButton() {
+	alert("hi");
 };
