14bc736016aca567b4df1a1a2ddf946802de3fae
arimeisels3@gmail.com <arimeisels3@gmail.com>
2020-11-25 19:36:57 -0500

    Keepvid Bookmarklet
    A bookmarklet that sends a YouTube video to the deep dark processors of Keepvid's website from which it will never return!!!! HAHAHAHAHAHAHAHAHAHA!!!!!! Occasioanl updates will be realeased as needed. Keepvid, you are free to use this if you give me (Ari Meisels) credit. And no offense but your broken bookmarklet sucks.

--- keepvid/bookmarklet.js
+++ keepvid/bookmarklet.js
@@ -1,14 +1,10 @@
-if ((window.location.host + window.location.pathname == "www.youtube.com/") || (window.location.host + window.location.pathname == "www.youtube.com")) {
-	alert("Do you know the difference between YouTube and a YouTube VIDEO????????");
-} else {
-	if (window.location.host + window.location.pathname == "www.youtube.com/watch") {
-		if (document.getElementById("reason") == null) {
-			var myWindow = window.open("https://keepvid.works/?url=" + window.location);
-			myWindow.document.getElementsByClassName("btn btn-lg btn-danger")[0].click();
-		} else {
-			alert("Error 404: The video you are looking for was lost, stolen, or may never have existed. But in all probability, it was probably taken down or made private. Unless you are trying to break my precious bookmarklet, in which case you should be ashamed of yourself!!!!!");
-		}
+if (window.location.host + window.location.pathname == "www.youtube.com/watch") {
+	if (document.getElementById("reason") == null) {
+		window.open("https://keepvid.works/?url=" + window.location);
+		alert("Click this bookmarklet after the page redirects to play the video.");
 	} else {
-		alert("Please click this on a YouTube video ONLY!!!!!!");
+		alert("Error 404: The video you are looking for was lost, stolen, or may never have existed. But in all probability, it was probably taken down or made private. Unless you are trying to break my precious bookmarklet, in which case you should be ashamed of yourself!!!!!");
 	}
+} else if (window.location.host == "keepvid.works") {
+	window.location.replace(document.getElementsByClassName("btn btn-lg btn-danger")[0].href);
 };
