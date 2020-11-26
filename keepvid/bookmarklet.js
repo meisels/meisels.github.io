04a24a5a5ad86a1d18c8dc80d5adf37044ea920e
arimeisels3@gmail.com <arimeisels3@gmail.com>
2020-11-25 21:33:15 -0500

    Keepvid Bookmarklet
    A bookmarklet that sends a YouTube video to the deep dark processors of Keepvid's website from which it will never return!!!! HAHAHAHAHAHAHAHAHAHA!!!!!! Occasioanl updates will be realeased as needed. Keepvid, you are free to use this if you give me (Ari Meisels) credit. And no offense but your broken bookmarklet sucks.

--- keepvid/bookmarklet.js
+++ keepvid/bookmarklet.js
@@ -1,16 +1,16 @@
-if (window.location.host + window.location.pathname == "www.youtube.com/watch") {
-	if (document.getElementById("reason") == null) {
-		alert("Click this bookmarklet after the page redirects to play the video.");
-		window.open("https://keepvid.works/?url=" + window.location);
+if (window.location.host + window.location.pathname == "www.youtube.com/watch") { // Checks if you are actually on YouTube
+	if (document.getElementById("reason") == null) { // Checks if the video exists or not
+		alert("Click this bookmarklet in the page that will open to play the video."); // Tells you to click the bookmarklet again
+		window.open("https://keepvid.works/?url=" + window.location); // Opens a new tab with Keepvid and the video you were just in
 	} else {
-		alert("Error 404: The video you are looking for was lost, stolen, or may never have existed. But in all probability, it was probably taken down or made private. Unless you are trying to break my precious bookmarklet, in which case you should be ashamed of yourself!!!!!");
+		alert("Error 404: The video you are looking for was lost, stolen, or may never have existed"); // If the video doesn't exist, you see this message.
 	}
-} else if (window.location.host == "keepvid.works") {
-	document.getElementsByClassName("btn btn-lg btn-danger")[0].click();
+} else if (window.location.pathname == "keepvid.works") { // Checks if you are on Keepvid
+	document.getElementsByClassName("btn btn-lg btn-danger")[0].click(); // Presses the download button (if there isn't one, nothing happens)
 } else {
-	var video = encodeURI(prompt("Paste the video URL here (if you didn't copy it yet, press cancel)."));
-	if (video.includes("youtube.com/watch")) {
-		alert("Click this bookmarklet after the page redirects to play the video.");
-		window.location.replace("https://keepvid.works/?url=" + video);
+	var video = encodeURI(prompt("Paste the video URL here (if you didn't copy it yet, press cancel).")); // If you are on a completely different site, like your email, the script will allow you to paste in the URL
+	if (video.includes("youtube.com/watch")) { // Checks if your link is a valid YouTube link
+		alert("Click this bookmarklet after the page redirects to play the video."); // Tells you to click the bookmarklet again
+		window.open("https://keepvid.works/?url=" + video); // Opens a new tab with Keepvid and the video you pasted
 	}
 };
