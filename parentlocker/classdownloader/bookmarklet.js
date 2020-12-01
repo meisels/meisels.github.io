9178571fe38c8c44e7543d338b48a5dcdf251220
arimeisels3@gmail.com <arimeisels3@gmail.com>
2020-11-30 21:30:16 -0500

    ParentLocker Class Downloader
    Since ParentLocker RUDELY removed the glorious download button, I had to figure out a way to reimpliment it. My goal was to recreate the button itself but this was way easier.

--- parentlocker/classdownloader/bookmarklet.js
+++ parentlocker/classdownloader/bookmarklet.js
@@ -10,7 +10,7 @@ if (done != 1 && window.location.hostname.match(/\w*\.\w*$/gi)[0] + window.locat
 	document.getElementsByClassName("noprint noprinttop")[0].id = "addexcel";
 	addId = document.getElementById("addexcel");
 	addId.insertBefore(x, addId.childNodes[2]);
-}; else if (done == 1 && window.location.hostname.match(/\w*\.\w*$/gi)[0] + window.location.pathname == "parentlocker.com/sis/directory.php" && new URL(location.href).searchParams.get('i') != null) {
+} else if (done == 1 && window.location.hostname.match(/\w*\.\w*$/gi)[0] + window.location.pathname == "parentlocker.com/sis/directory.php" && new URL(location.href).searchParams.get('i') != null) {
 
 	window.location.replace("javascript:window.location.replace(window.location + '&media=excel');");
 
