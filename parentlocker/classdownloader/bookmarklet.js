dadb6a426d61ba72f71ae67cabc37ff5f2453212
arimeisels3@gmail.com <arimeisels3@gmail.com>
2020-11-30 19:54:45 -0500

    ParentLocker Class Downloader
    Since ParentLocker RUDELY removed the glorious download button, I had to figure out a way to reimpliment it. My goal was to recreate the button itself but this was way easier.

--- parentlocker/classdownloader/bookmarklet.js
+++ parentlocker/classdownloader/bookmarklet.js
@@ -1,10 +1,13 @@
-var x = document.createElement("A");
-var elem = document.createElement("img");
-var t = document.createTextNode("Excel");
-elem.setAttribute("src", "https://www.parentlocker.com/images/page_white_excel.png");x.setAttribute("href", "javascript:window.location.replace(window.location + '&media=excel');");
-x.setAttribute("class", "toplink");
-x.appendChild(elem);
-x.appendChild(t);
-document.getElementsByClassName("noprint noprinttop")[0].id = "addexcel";
-addId = document.getElementById("addexcel");
-addId.insertBefore(x, addId.childNodes[2]);
+if (done != 1 && window.location.hostname.match(/\w*\.\w*$/gi)[0] + window.location.pathname == "parentlocker.com/sis/directory.php" && new URL(location.href).searchParams.get('i') != null) {
+	var done = 1;
+	var x = document.createElement("A");
+	var elem = document.createElement("img");
+	var t = document.createTextNode("Excel");
+	elem.setAttribute("src", "https://www.parentlocker.com/images/page_white_excel.png");x.setAttribute("href", "javascript:window.location.replace(window.location + '&media=excel');");
+	x.setAttribute("class", "toplink");
+	x.appendChild(elem);
+	x.appendChild(t);
+	document.getElementsByClassName("noprint noprinttop")[0].id = "addexcel";
+	addId = document.getElementById("addexcel");
+	addId.insertBefore(x, addId.childNodes[2]);
+};
