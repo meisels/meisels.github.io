e7f0871e0a6101a4c545d5defc2a56e68ffd903a
arimeisels3@gmail.com <arimeisels3@gmail.com>
2020-12-01 19:55:17 -0500

    ParentLocker Class Downloader
    Since ParentLocker RUDELY removed the glorious download button, I had to figure out a way to reimpliment it.

--- parentlocker/classdownloader/bookmarklet.js
+++ parentlocker/classdownloader/bookmarklet.js
@@ -1,15 +1,15 @@
 if (done != 1 && window.location.hostname.match(/\w*\.\w*$/gi)[0] + window.location.pathname == "parentlocker.com/sis/directory.php" && new URL(location.href).searchParams.get('i') != null && document.body.innerHTML != "Unauthorized Access.") {
 	var done = 1;
-	var x = document.createElement("A");
-	var elem = document.createElement("img");
-	var t = document.createTextNode("Excel");
-	elem.setAttribute("src", "https://www.parentlocker.com/images/page_white_excel.png");x.setAttribute("href", "javascript:window.location.replace(window.location + '&media=excel');");
-	x.setAttribute("class", "toplink");
-	x.appendChild(elem);
-	x.appendChild(t);
-	document.getElementsByClassName("noprint noprinttop")[0].id = "addexcel";
-	addId = document.getElementById("addexcel");
-	addId.insertBefore(x, addId.childNodes[2]);
+	var buttonLink = document.createElement("a");
+	var buttonPicture = document.createElement("img");
+	var buttonText = document.createTextNode("Excel");
+	elem.setAttribute("src", "../images/page_white_excel.png");x.setAttribute("href", "javascript:window.location.replace(window.location + '&media=excel');");
+	buttonLink.setAttribute("class", "toplink");
+	buttonLink.appendChild(buttonPicture);
+	buttonLink.appendChild(buttonText);
+	document.getElementsByClassName("noprint noprinttop")[0].id = "addExcel";
+	addId = document.getElementById("addExcel");
+	addId.insertBefore(buttonLink, addId.childNodes[2]);
 } else if (done == 1 && window.location.hostname.match(/\w*\.\w*$/gi)[0] + window.location.pathname == "parentlocker.com/sis/directory.php" && new URL(location.href).searchParams.get('i') != null) {
 
 	window.location.replace("javascript:window.location.replace(window.location + '&media=excel');");
