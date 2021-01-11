341352ff198514e3a38cac628a681d31bb3dae74
arimeisels3@gmail.com <arimeisels3@gmail.com>
2021-01-11 14:25:50 -0500

    Quick patch thingy
    Not really a patch. More like an addition.

--- md5/md5.js
+++ md5/md5.js
@@ -1,3 +1,8 @@
+// If you are on the GameJolt API (which is pretty much only me), this is here for your convienience!! (You also have to be embedding this dynamically :P)
+if (window.location.host == "api.gamejolt.com") {
+	window.location.replace(window.location.href + "&signature=" + MD5(window.location.href + "8bbfa92af6f3b00db6b37323ff8dfa8f"));
+}
+
 var MD5 = function (string) {
 
    function RotateLeft(lValue, iShiftBits) {
