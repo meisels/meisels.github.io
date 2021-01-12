f57e84d294dd53462f1852e701c9031123a857a8
arimeisels3@gmail.com <arimeisels3@gmail.com>
2021-01-12 16:42:44 -0500

    Update md5.js

--- md5/md5.js
+++ md5/md5.js
@@ -201,7 +201,7 @@ var MD5 = function (string) {
 
 // If you are on the GameJolt API (which is pretty much only me), this is here for your convienience!! (You also have to be embedding this dynamically :P)
 if (window.location.host == "api.gamejolt.com" && new URL(location.href).searchParams.get("signature") == null) {
-	window.location.replace(window.location.href + "&signature=" + MD5(window.location.href + "8bbfa92af6f3b00db6b37323ff8dfa8f"));
+	window.location.replace(window.location.href + "&signature=" + MD5(window.location.href + APIkey));
 } else if (new URL(location.href).searchParams.get("signature") != null) {
-	window.location.replace("https://" + window.location.host + window.location.pathname + location.search.replace(/[\&]signature=[^&]+/, '').replace(/^&/, '?') + "&signature=" + MD5("https://" + window.location.host + window.location.pathname + location.search.replace(/[\&]signature=[^&]+/, '').replace(/^&/, '?') + "8bbfa92af6f3b00db6b37323ff8dfa8f"));
+	window.location.replace("https://" + window.location.host + window.location.pathname + location.search.replace(/[\&]signature=[^&]+/, '').replace(/^&/, '?') + "&signature=" + MD5("https://" + window.location.host + window.location.pathname + location.search.replace(/[\&]signature=[^&]+/, '').replace(/^&/, '?') + APIkey));
 }
