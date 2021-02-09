de82f7652cef57e1cfa896b757dec857ac29f65b
arimeisels3@gmail.com <arimeisels3@gmail.com>
2021-02-08 22:26:07 -0500

    Fixed bug
    Click either of the PRINT buttons (you can click cancel afterwards) and then try to download the spreadsheet. The reason for this bug is because the print window adds a # to the URL after closing. The # is used for jumping to a certain element on the page by using its id. Because it is there, my script adds '&media=excel' after the # so it reads it as 'jump to the element with the id &media=excel'. So because there is none, it does nothing. I am such a terrible programmer :cry: .

--- parentlocker/classdownloader/bookmarklet4.js
+++ parentlocker/classdownloader/bookmarklet4.js
@@ -1,9 +1,10 @@
-if (window.location.hostname.match(/\w*\.\w*$/gi)[0] + window.location.pathname == "parentlocker.com/sis/directory.php" && new URL(location.href).searchParams.get('i') != null && document.body.innerHTML != "Unauthorized Access.") {
+if (done != 1 && window.location.hostname.match(/\w*\.\w*$/gi)[0] + window.location.pathname == "parentlocker.com/sis/directory.php" && new URL(location.href).searchParams.get('i') != null && document.body.innerHTML != "Unauthorized Access.") {
+	var done = 1;
 	var buttonLink = document.createElement("a");
 	var buttonPicture = document.createElement("img");
 	var buttonText = document.createTextNode("Excel");
 	buttonPicture.setAttribute("src", "../images/page_white_excel.png");
-	buttonLink.setAttribute("href", "javascript:window.location.replace(window.location.protocol + '//' + window.location.host + window.location.pathname + window.location.search + '&media=excel');");
+	buttonLink.setAttribute("href", window.location.href.substring(window.location.href.lastIndexOf('/')+1) + "&media=excel");
 	buttonLink.setAttribute("class", "toplink");
 	buttonLink.appendChild(buttonPicture);
 	buttonLink.appendChild(buttonText);
